0325b66a3b787de4b5b03d93c5b0dde1821c6f4a
Sergiu1-cyber <manzasergiu@gmail.com>
2024-03-09 10:41:09 +0200

    redactez 0 forma

--- components/Form.tsx
+++ components/Form.tsx
@@ -4,21 +4,30 @@ export default function Form() {
 
 
   return (
-    <form>
-      <h3>Form</h3>
-      <label htmlFor="cistig">Cistig</label>
-      <input type="radio" id="cistig" name="operatia" value="cistig" />
-      <label htmlFor="cheltui">Cheltui</label>
-      <input type="radio" id="cheltui" name="operatia" value="cheltui" />
-      <label htmlFor="tipul"></label>
-      <select name="" id="tipul">
-        <option value=""></option>
+    <form id="form_1" className="border-2 border-solid border-blue-500 p-5">
+      <div className="flex flex-col">
+        <h3>Form</h3>
+        <div className="flex">
+          <div className="flex flex-col mb-2 mr-4">
+            <label htmlFor="cistig">Cistig</label>
+            <input type="radio" id="cistig" name="operatia" value="cistig" />
+          </div>
+          <div className="flex flex-col mb-2">
+            <label htmlFor="cheltui">Cheltui</label>
+            <input type="radio" id="cheltui" name="operatia" value="cheltui" />
+          </div>
+        </div>
+      <label htmlFor="tipul">Tipul</label>
+      <select name="tipul" id="tipul" form="form_1" className="mb-2">
+        <option >operatia</option>
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
       </select>
       <label htmlFor="suma">Suma</label>
-      <input type="number" id="suma" />
+      <input type="number" id="suma" name="suma" placeholder="suma" className="border-2 border-solid border-black mb-2"/>
+      <input type="submit" value="incarc" className="bg-blue-500" />
+      </div>
     </form>
   )
 }
