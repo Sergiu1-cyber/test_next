345f0e14700fe4c1de59bcb8ecd191397ee9f388
Sergiu1-cyber <manzasergiu@gmail.com>
2024-03-15 23:40:54 +0200

    delete mongodb

--- app/api/operatii/route.ts
+++ app/api/operatii/route.ts
@@ -1,20 +1,9 @@
-import connectDB from "@/actions/mongodb/connectDB"
-import Operatii from "@/actions/mongodb/models/operatia"
 import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
     const data = await req.json()
 
-    try {
-        await connectDB()
-        const op = new Operatii(data)
-        console.log(op)
-        await op.save()
-        return NextResponse.json({msg: "date salvate"})
-    } catch(e) {
-        console.log(`Error in route.js : ${e}`)
-    }
-
+    return NextResponse.json({msg: data})
 }
 
 
